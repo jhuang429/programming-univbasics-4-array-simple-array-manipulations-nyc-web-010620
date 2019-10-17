505b37d6a1cab71d43e86b44043f3ea9579a8f14
Jerry Huang <jhuang9240@gmail.com>
2019-10-17 11:42:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,31 @@
 def using_push(array, element)
   updated_array = array.push(element)
 end
+
+def using_unshift(array,string)
+  updated_array = array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_unshift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  array.concat(arr2)
+end
+
+def using_insert(arr,element)
+  array.insert(element)
+end
