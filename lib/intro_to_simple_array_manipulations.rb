4a574d8c64bd191476be90f177a5c12b885cfe67
Jerry Huang <jhuang9240@gmail.com>
2019-10-17 11:45:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,19 @@ end
 def using_insert(arr,element)
   array.insert(element)
 end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten()
+end
+
+def using_delete(arr,string)
+  arr.delete(string)
+end
+
+def using_delete_at(arr, int)
+  arr.delete(int)
+end
